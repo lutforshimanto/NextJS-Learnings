f33bb6933bb05f13e861de89b3823e1b52cda982
lutforshimanto <lutfor.shimanto2041@gmail.com>
2024-07-09 09:27:15 +0600

    Heading updated.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <></>
+    <>
+      <div className="h-screen bg-yellow-100">
+        <h1 className="text-3xl">
+          Home Page
+        </h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur, amet. Suscipit ullam, quia vero laudantium culpa iure. Veritatis, totam est explicabo dicta beatae consectetur eos qui quaerat provident quis hic soluta, libero distinctio aliquid ipsa ullam rem minima eligendi nisi architecto molestiae quibusdam quas velit. Doloribus dolor minus amet, molestias nesciunt eum. Impedit, commodi? Quis ipsa libero modi dolores, quas ipsam tempore facere cupiditate similique deserunt sed, animi nisi. Modi consequuntur nostrum deleniti sunt in possimus rem! Ut laudantium quaerat aliquid ullam non rerum libero pariatur quia quo. Labore corporis illum, suscipit, sit vitae sed assumenda laboriosam quae aperiam, ullam dicta quidem cum repellat dolore magni esse odio inventore beatae fuga accusantium deserunt blanditiis aut aliquam perspiciatis. Neque, minus corrupti. Sequi, minima? Ipsa tempore perferendis iure reiciendis possimus voluptatem repudiandae rerum sequi adipisci odio exercitationem fuga, odit, eius nulla voluptas consequuntur placeat laboriosam tempora et, omnis quibusdam. Reprehenderit obcaecati repellendus explicabo mollitia ducimus culpa, doloribus, fuga velit aliquid asperiores quia recusandae vero laborum saepe odio ipsam. Labore.</p>
+      </div>
+    </>
   );
 }
